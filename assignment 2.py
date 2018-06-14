7586075ebb8c049fa1f86b23f91ec297f74a163f
rishabhthakur1809 <40190979+rishabhthakur1809@users.noreply.github.com>
2018-06-14 10:30:56 +0530

    assignment 2 added

---
+++ assignment 2.py
@@ -0,0 +1,32 @@
+#question 1
+
+print ("hello")
+
+#question 2
+
+a="acad"
+b="view"
+print(a+b)
+
+#question  3
+
+x=input("Enter the name")
+y=input("Enter the  age")
+z=input("Enter the phone number")
+print(x)
+print(y)
+print(z)
+
+#question 4
+print("let\'s get started")
+
+#question5
+s="acadview"
+course="python"
+fees=5000
+print("welcome to the %s & %s is vrey good language and the fees is %d"%(s,course,fees))
+
+#qustion6
+name="Tony Stark"
+salary=1000000
+print('%s"%d'%(name,salary))
