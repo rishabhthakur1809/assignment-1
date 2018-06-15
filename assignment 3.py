c43cc2e1556d88dd9082cc5cfa0058a99fc18b52
rishabhthakur1809 <40190979+rishabhthakur1809@users.noreply.github.com>
2018-06-15 09:28:32 +0530

    assignment 3 added

---
+++ assignment 3.py
@@ -0,0 +1,38 @@
+#question 1
+a=input("Enter the number")
+b=input("Enter the float number")
+c=input("Enter the number")
+d=[ ]
+d.append(a)
+d.append(b)
+d.append(c)
+print(d)
+
+ #question
+i=["google","apple","facebook","microsoft","tesla"]
+d.append(i)
+print(d)
+
+#question 3
+k=[1,2,3,4,5,2,4,1,5,2,2,1]
+print(k.count(2))
+print(k)
+
+#question 4
+l=[1,4,5,3,2]
+l.sort()
+print(l)
+
+#question 5
+g=[1,4,2,7,9,8]
+h=[6,5,2,8]
+e=[g+h]
+e.sort()
+print(e)
+
+#question 6
+r=input("Enter the number")
+l.append(r)
+print(r)
+l.pop( )
+print(r)
